a93886e2dbe3d853cf951f211a942f08c6e5e4c8
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2024-03-01 18:57:42 +0000

    implicit and explicit

---
+++ casting.java
@@ -0,0 +1,14 @@
+public class casting {
+    public static void main(String[] args) {
+        //inplicit casting: smaller datatype is casted into larger datatype
+        double a=109.5;
+        double final_a=a+18;
+        System.out.println(a);
+        System.out.println(final_a);
+
+        //explicit casting: larger datatype is casted into smaller datatype
+        int b=(int)198.8;
+        System.out.println(b);
+    }
+    
+}
