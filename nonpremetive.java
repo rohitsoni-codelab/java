5e858d3aa4c2d2f8b9724c2568070783ba137771
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2024-03-01 17:35:29 +0000

    operation on string

--- nonpremetive.java
+++ nonpremetive.java
@@ -1,4 +1,30 @@
 public class nonpremetive {
-    public static void main
+    public static void main(String[] args) {
+        //method 1
+        String name="krishna";
+        
+        //method 2
+        String name1 =new String("krish");
+        
+        //printing part
+        System.out.println(name);
+        System.out.println(name1);
+
+        //length
+        System.out.println(name.length());
+        System.out.println(name1.length());
+        
+        //concating
+        String name2=name+" and "+name1;
+        System.out.println(name2);
+
+        //charat
+        System.out.println(name.charAt(3));
+
+        //replace
+        String name4=name;
+        System.out.println(name4.replace('n', 'o'));
+
+    }
     
 }
