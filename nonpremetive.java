a04c0c073ef6ddc32752bd67b727bff39082ed92
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2024-03-01 16:57:59 +0000

    basics

---
+++ nonpremetive.java
@@ -0,0 +1,4 @@
+public class nonpremetive {
+    public static void main
+    
+}
---
+++ premetive.java
@@ -0,0 +1,10 @@
+public class premetive {
+    public static void main(String[] args) {
+        int age=30;
+        char letter='$';
+        long phone=1234567890;
+        String name="kanhiya";
+        System.out.println("age "+age+"letter "+letter+"phone "+phone+"name "+name);
+    }
+    
+}
