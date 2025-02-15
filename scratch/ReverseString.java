5df3957cff08933c9202368da645c0e2dce989f9
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-15 14:49:23 +0000

    case 2

--- scratch/ReverseString.java
+++ scratch/ReverseString.java
@@ -10,5 +10,15 @@ public class ReverseString {
         }
         System.out.println(s2);
 
+
+
+        String str1="Rohit Soni";
+        String str2="";
+        String sarr[]=str1.split(" ");
+        for(int i=sarr.length-1;i>=0;i--)
+        {
+            str2=str2+sarr[i]+" ";
+        }
+        System.out.println(str2);
     }
 }
