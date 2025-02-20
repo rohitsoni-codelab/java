7ca8b48221c269b9f42df6add6a00efa50d0f0f6
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-20 18:29:07 +0000

    removed comments

--- scratch/TryCatch1.java
+++ scratch/TryCatch1.java
@@ -4,10 +4,10 @@ public class TryCatch1 {
     public static void main(String[] args) {
         int a = 2;
         int b = 0;
-        int c = 0;  // Initialize c to avoid errors
+        int c = 0;  
         
         try {
-            c = a / b; // This will throw an ArithmeticException
+            c = a / b; 
         } catch (Exception e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }
