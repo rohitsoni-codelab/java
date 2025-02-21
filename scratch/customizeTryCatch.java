bd035802822df71f881dd7ee9275033ccdced054
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-21 18:28:50 +0000

    customize exception

---
+++ scratch/customizeTryCatch.java
@@ -0,0 +1,25 @@
+package scratch;
+
+public class customizeTryCatch {
+    public static void main(String[] args) {
+        int a=6;
+        int b=-2;
+        int c;
+
+        try{
+            if(b<=0)
+            {
+                Exception ex= new ArithmeticException();
+                throw ex;
+            }
+            c=a/b;
+            System.out.println("Value"+c);
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Enter a non negative number");
+        }
+        
+    }
+    
+}
