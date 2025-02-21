3c643e63f37b3f4cd76b117ad53bd096425d4f18
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-21 17:49:41 +0000

    using finally block for the compulsary statements either for exception or normal

---
+++ scratch/tryCatchWithFinally.java
@@ -0,0 +1,20 @@
+package scratch;
+import java.util.*;
+public class tryCatchWithFinally {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        try{
+            System.out.println("Enter=");
+            int num=sc.nextInt();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Use only integer");
+        }
+        finally{
+            sc.close();
+            System.out.println("Closing the Scanner class");
+        }
+        System.out.println("Ending the application");
+    }
+}
