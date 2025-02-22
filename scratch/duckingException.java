e1b0359161a165be4fff8e769a30bfbe5bbfb626
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-22 18:15:56 +0000

    ducking exception

---
+++ scratch/duckingException.java
@@ -0,0 +1,32 @@
+package scratch;
+
+public class duckingException {
+
+	void m() 
+	{ 
+		int data = 50 / 0; 
+	} 
+
+	void n() 
+	{ 
+		m();  
+	} 
+
+	void p() 
+	{ 
+		try { 
+			n(); 
+		} 
+		catch (Exception e) { 
+			System.out.println("Exception handled"); 
+		} 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		duckingException obj = new duckingException(); 
+		obj.p(); 
+		System.out.println("Normal flow..."); 
+	} 
+} 
+
