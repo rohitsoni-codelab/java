208fc8457b35b97508c9f4caddd018fe6567c070
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-25 18:13:50 +0000

    using both the methods of creating the thread

---
+++ scratch/thread_combination.java
@@ -0,0 +1,52 @@
+package scratch;
+import java.io.*;
+import java.util.*;
+
+
+// Running the Threads in Java
+
+
+// Method 1 - Thread Class
+class ThreadImpl extends Thread
+{
+      // Method to start Thread
+      @Override
+      public void run()
+    {
+          String str = "Thread Class Implementation Thread"
+                      + " is Running Successfully";
+        System.out.println(str);
+    }
+}
+
+// Method 2 - Runnable Interface
+class RunnableThread implements Runnable 
+{    
+      // Method to start Thread
+      @Override
+    public void run()
+    {
+          String str = "Runnable Interface Implementation Thread"
+                      + " is Running Successfully";
+        System.out.println(str);
+    }
+
+}
+
+public class thread_combination
+{
+    public static void main(String[] args)
+    {
+          
+          // Method 1 - Thread Class
+          ThreadImpl t1 = new ThreadImpl();
+          t1.start();
+      
+          // Method 2 - Runnable Interface
+        RunnableThread g2 = new RunnableThread();
+        Thread t2 = new Thread(g2);
+          t2.start();
+          
+    }
+    
+}
