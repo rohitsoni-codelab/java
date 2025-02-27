3e67ab11852e7d8c3d4447134e41e50246cfb9bd
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-27 06:51:25 +0000

    gitting a bug

--- scratch/Thread/IsAlive_join.java
+++ scratch/Thread/IsAlive_join.java
@@ -1,14 +1,14 @@
 package scratch.Thread;
 
-import scratch.palindromString;
+// import scratch.palindromString;
 
 class PrintingThread extends Thread {
     public void run() {
         System.out.println("-----------printing thread-------------");
         try {
-            for (int i = 0; i <= 3; i++) 
+            for (int i = 0; i <= 2; i++) 
             {
-                System.out.println("Chappak...");
+                System.out.println("Chappak..."+(i+1));
                 Thread.sleep(2000);
             }
         } 
@@ -20,13 +20,34 @@ class PrintingThread extends Thread {
     }
 
 }
-
+class PrintingThread1 extends Thread{
+    public void run()
+    {
+        System.out.println("-----------Writing Thread-------------");
+        try {
+            for (int i = 0; i <= 2; i++) 
+            {
+                System.out.println("Tappak..."+(i+1));
+                // Thread.sleep(2000);
+            }
+        } 
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            System.out.println("Getting some Error");
+            e.printStackTrace();
+        }
+    }
+}
 public class IsAlive_join {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         PrintingThread t1=new PrintingThread();
-        t1.isAlive();
+        PrintingThread1 t2=new PrintingThread1();
+
+        System.out.println(t1.isAlive()); 
         t1.start();
-        t1.isAlive();
+        t1.join();
+        t2.start();
+        System.out.println(t1.isAlive()); // why this line is executed before the t2.start
     }
 
 }
