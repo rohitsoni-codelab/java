d69bd798863f6fe0747acbaabec3a7ac3babfbfa
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-28 16:40:54 +0000

    works like a complete tree

---
+++ scratch/Collections/TreeSetExample.java
@@ -0,0 +1,16 @@
+package scratch.Collections;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        TreeSet ts=new TreeSet();
+        ts.add(25);
+        ts.add(100);
+        ts.add(75);
+        ts.add(12);
+
+        System.out.println(ts);
+    }
+    
+}
