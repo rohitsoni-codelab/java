64d13748de62b079f53d107c103427ba62f2bf68
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-28 16:36:44 +0000

    works as min heap

---
+++ scratch/Collections/PriorityQueueExample.java
@@ -0,0 +1,17 @@
+package scratch.Collections;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        PriorityQueue pq=new PriorityQueue();
+        pq.add(25);
+        pq.add(100);
+        pq.add(75);
+        pq.add(12);
+
+        System.out.println(pq);
+
+    }
+    
+}
