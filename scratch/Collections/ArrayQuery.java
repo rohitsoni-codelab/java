8ab5f88f4e7a51505f9d5daae3fd54841c28d8dd
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-02-28 16:31:14 +0000

    works like a DoubleEndedQueue

---
+++ scratch/Collections/ArrayQuery.java
@@ -0,0 +1,17 @@
+package scratch.Collections;
+
+import java.util.ArrayDeque;
+
+public class ArrayQuery {
+    public static void main(String[] args) {
+        ArrayDeque ad=new ArrayDeque();
+        ad.add(25);
+        ad.add(100);
+        ad.add(75);
+        ad.add(12);
+
+        System.out.println(ad);
+        
+    }
+    
+}
