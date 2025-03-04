b827983f24bec1e1d83d08387c277c51f0b48600
Rohit Soni <138715418+rohitsoni-codelab@users.noreply.github.com>
2025-03-04 20:30:40 +0000

    added user input as string

--- scratch/constructorOverloading.java
+++ scratch/constructorOverloading.java
@@ -1,27 +1,28 @@
 package scratch;
-class Demo{
+import java.util.*;
+class Demo0{
     int age;
     String name;
 
-    public Demo()
+    public Demo0()
     {
         System.out.println("Zero parameterized constructor");
         age=19;
         name="Rohit";
     }
-    public Demo(int age)
+    public Demo0(int age)//constructor overloading
     {
         System.out.println("age parameter constructor");
         this.age=age;
         name="Shubham";
     }
-    public Demo(int age, String name)
+    public Demo0(int age, String name)//constructor overloading
     {
         System.out.println("age and name parameter constructor");
         this.age=age;
         this.name=name;
     }
-    public void disp()
+    public void disp()// method
     {
         System.out.println(age);
         System.out.println(name);
@@ -29,12 +30,20 @@ class Demo{
 }
 public class constructorOverloading {
     public static void main(String[] args) {
-        Demo d1=new Demo();
-        d1.disp();
-        Demo d2=new Demo(23);
-        d1.disp();
-        Demo d3=new Demo(34,"raj");
-        d1.disp();
+        Scanner sc=new Scanner(System.in);//--> user input lene ke liye scanner class banaye...
+        Demo0 d1=new Demo0();
+        d1.disp();//-->
+        Demo0 d2=new Demo0(23);
+        d2.disp();
+        Demo0 d3=new Demo0(34,"raj");
+        d3.disp();
+
+
+        System.out.print("Enter any name=");
+        String naam=sc.next();//user input liyeee...
+
+        Demo0 d5=new Demo0(20, naam);//
+        d5.disp();
 
     }
     
